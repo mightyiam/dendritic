d336b0fbb8d4186db8e3b8b430b62cbfdc28eed4
Mariappan Ramasamy <1221719+nappairam@users.noreply.github.com>
2026-01-03 06:57:10 +0800

    add minimal config needed for nixosConfiguration
    Fixing error:
    evaluation warning: system.stateVersion is not set, defaulting to 25.11. Read why this matters on https://nixos.org/manual/nixos/stable/options.html#opt-system.stateVersion.
    error:
           … while checking flake output 'checks'
    
           … while checking the derivation 'checks.x86_64-linux."configurations:nixos:desktop"'
    
           … while evaluating the attribute 'optionalValue.value'
             at /nix/store/qzm4sksyx1q1g872xxkr1160hhqlk9nm-source/lib/modules.nix:1256:5:
             1255|
             1256|     optionalValue = if isDefined then { value = mergedValue; } else { };
                 |     ^
             1257|   };
    
           … while evaluating definitions from `/nix/store/ndfvbqznn062jfg4wbc181lw6si5c90p-source/example/modules/nixos.nix':
    
           … while evaluating the option `system.build.toplevel':
    
           … while evaluating definitions from `/nix/store/qzm4sksyx1q1g872xxkr1160hhqlk9nm-source/nixos/modules/system/activation/top-level.nix':
    
           (stack trace truncated; use '--show-trace' to show the full, detailed trace)
    
           error:
           Failed assertions:
           - The ‘fileSystems’ option does not specify your root file system.
           - You must set the option ‘boot.loader.grub.devices’ or 'boot.loader.grub.mirroredBoots' to make the system bootable.
           - Exactly one of users.users.iam.isSystemUser and users.users.iam.isNormalUser must be set.
    
           - users.users.iam.group is unset. This used to default to
           nogroup, but this is unsafe. For example you can create a group
           for this user with:
           users.users.iam.group = "iam";
           users.groups.iam = {};

--- example/modules/desktop.nix
+++ example/modules/desktop.nix
@@ -11,5 +11,11 @@ in
       # ...other `nixos` modules
     ];
     nixpkgs.hostPlatform = "x86_64-linux";
+
+    # Minimal required NixOS options for evaluation
+    system.stateVersion = "25.11";
+    fileSystems."/".device = "/dev/sda1";
+    boot.loader.grub.devices = [ "/dev/sda" ];
+    users.users.${config.username}.isNormalUser = true;
   };
 }
